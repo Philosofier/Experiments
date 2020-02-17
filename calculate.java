d80fca80e754838602d3bbe316cdecd5505e6214
philosofier <philosofier@gmail.com>
2020-02-17 22:22:39 +0700

    done

--- calculate.java
+++ calculate.java
@@ -2,5 +2,22 @@ public class calculate {
 public static void main (String[] args)
 {
 System.out.println("Hello world!");
+int first = 254;
+int second = 898;
+int summ = first + second;
+System.out.println("summ  = " + summ);
+double a = 12.0;
+int b = 567;
+double d = a + b;
+System.out.println("d = " + d);
+short c = 345;
+short f = 678;
+float j = c - f;
+System.out.println("j = " + j);
+double r = 89;
+float l = 15;
+double p = r/l;
+System.out.println("p = " +p);
+ 
 }
 }
